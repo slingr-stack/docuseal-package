416cdd9851217dec47cca74dd69132d3fd5060a8
Diego Gaviola <dgaviola@slingr.io>
2024-09-05 12:44:12 -0300

    Fixing webhooks

--- listeners/webhook.js
+++ listeners/webhook.js
@@ -15,7 +15,7 @@ listeners.defaultWebhookDocuSeal = {
     callback: function(event) {
         let headers = event.data.headers;
         let secret = headers["X-DocuSeal-Secret"] || headers["x-docuseal-secret"];
-        if (pkg.stripe.utils.verifySecret(secret)) {
+        if (pkg.docuseal.utils.verifySecret(secret)) {
             sys.events.triggerEvent("docuseal:webhook", event.data);
         } else {
             sys.logs.warn('[docuseal] Invalid secret for webhook');
